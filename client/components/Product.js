f596a4e04cc9e64b47afe9ed13622e7a23bf2092
Simeon Lee <simeonhlee@gmail.com>
2016-12-02 02:27:03 -0800

    Update class names in productjs

--- client/components/Product.js
+++ client/components/Product.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Product = props => {
   console.log(props);
+  var price = '' + props.defaultPriceInCents;
+  price = '$' + price.slice(0, price.length - 2) + '.' + price[price.length - 2] + price[price.length - 1];
   return (
-    <div className="product">
-      <div className="product-name">{props.name}</div>
-      <div className="product-price">{props.defaultPriceInCents}</div>
-      <img className="product-image" src={props.mainImage.ref} />
+    <div className="product-list-item">
+      <div className="product-list-item-image">
+        <img src={props.mainImage.ref} />
+      </div>
+      <div className="product-list-item-text">{props.name}</div>
+      <div className="product-list-item-text">{price}</div>
     </div>
   )
 }
