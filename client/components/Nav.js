1f45fc97b795184dd6de0da7d67f94d08b852218
Simeon Lee <simeonhlee@gmail.com>
2016-12-03 01:04:58 -0800

    Implement logo (logo recreated in Sketch app)

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../images/fiji-logo@2x.png';
 
 const Nav = props => {
   return (
     <div className="nav">
-      <div className="nav-text logo">{props.pageTitle.split(' | ')[1]}</div>
+      <div className="logo"><img src={logo} /></div>
       <div className="nav-text nav-selection">Water</div>
       <div className="nav-text nav-selection">Extras</div>
       <div className="nav-text nav-selection">Company</div>
