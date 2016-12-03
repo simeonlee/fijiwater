469a40d68dbb92fc41d3dcc71f3ad4449967018d
Simeon Lee <simeonhlee@gmail.com>
2016-12-03 03:33:27 -0800

    Update and clean up navigation animation

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -4,27 +4,15 @@ import logoNeg from '../images/fiji-logo-negative@2x.png';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // ES6
 
 const Nav = props => {
-  const bgStyle = {
-    backgroundColor:'rgba(255, 255, 255, 0.8)',
-    // backgroundColor: props.transformed ? 'rgba(255, 255, 255, 0.8)' : 'rgba(255, 255, 255, 0)',
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-    zIndex: '-99',
-    top: '0',
-    left: '0',
-  };
-  // const textStyle = {
-  //   color: black,
-  // };
   return (
     <div className={'nav' + (props.transformed ? ' transformed' : ' untransformed')}>
-      {props.transformed ? <div style={bgStyle}></div> : null}
-      <div className="logo wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s"><img src={props.transformed ? logoNeg : logo} /></div>
-      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Water</div>
-      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Extras</div>
-      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Company</div>
-      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Contact</div>
+      <div className="logo wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+        <img className={'logo-img' + (props.transformed ? ' transformed' : ' untransformed')} src={props.transformed ? logoNeg : logo} />
+      </div>
+      <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Water</div>
+      <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Extras</div>
+      <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Company</div>
+      <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Contact</div>
     </div>
   )
 }
