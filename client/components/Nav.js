053ef91fa7d73a3a9193ee23eb6d92ef32a1c61b
Simeon Lee <simeonhlee@gmail.com>
2016-12-03 03:07:58 -0800

    Add nav bar negative transform upon scroll ability

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../images/fiji-logo@2x.png';
+import logoNeg from '../images/fiji-logo-negative@2x.png';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // ES6
 
 const Nav = props => {
@@ -19,11 +20,11 @@ const Nav = props => {
   return (
     <div className={'nav' + (props.transformed ? ' transformed' : ' untransformed')}>
       {props.transformed ? <div style={bgStyle}></div> : null}
-      <div className="logo wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s"><img src={logo} /></div>
-      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' black' : '')} data-wow-duration="0.5s" data-wow-delay="1s">Water</div>
-      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' black' : '')} data-wow-duration="0.5s" data-wow-delay="1s">Extras</div>
-      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' black' : '')} data-wow-duration="0.5s" data-wow-delay="1s">Company</div>
-      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' black' : '')} data-wow-duration="0.5s" data-wow-delay="1s">Contact</div>
+      <div className="logo wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s"><img src={props.transformed ? logoNeg : logo} /></div>
+      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Water</div>
+      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Extras</div>
+      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Company</div>
+      <div className={'nav-text nav-selection wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Contact</div>
     </div>
   )
 }
