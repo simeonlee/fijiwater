4e24172cb0fa92478bd0c319618a06836126496c
Simeon Lee <simeonhlee@gmail.com>
2016-12-03 13:16:39 -0800

    Implement responsive and animated shopping cart icon

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import logo from '../images/fiji-logo@2x.png';
-import logoNeg from '../images/fiji-logo-negative@2x.png';
+import cart from '../images/cart-icon@2x.png';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // ES6
 
 const Nav = props => {
   return (
     <div className={'nav' + (props.transformed ? ' transformed' : ' untransformed')}>
-      <div className="logo wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
-        <img className={'logo-img' + (props.transformed ? ' transformed' : ' untransformed')} src={logo} />
+      <div className="nav-left">
+        <div className="logo wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+          <img className={'logo-img' + (props.transformed ? ' transformed' : ' untransformed')} src={logo} />
+        </div>
+        <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Water</div>
+        <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Extras</div>
+        <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Company</div>
+        <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Contact</div>
       </div>
-      <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Water</div>
-      <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Extras</div>
-      <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Company</div>
-      <div className={'nav-text wow fadeInLeft' + (props.transformed ? ' transformed' : ' untransformed')} data-wow-duration="0.5s" data-wow-delay="1s">Contact</div>
+      <div className="cart wow fadeIn" data-wow-duration="0.5s" data-wow-delay="0.5s">
+        <img className={'cart-img' + (props.transformed ? ' transformed' : ' untransformed')} src={cart} />
+      </div>
     </div>
   )
 }
